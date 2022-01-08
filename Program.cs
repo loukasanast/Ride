92af6a38b44aeb0702170061ac36dbf5477b8812
Loukas Anastasiou <loukas@anastasiou.io>
2022-01-08 06:12:23 +0100

    Refactored class

--- Program.cs
+++ Program.cs
@@ -77,24 +77,4 @@ namespace Ride
             }
         }
     }
-
-    class ErrorHandler
-    {
-        public static void HandleError(string message)
-        {
-            Console.WriteLine("An error occured");
-            Console.WriteLine(message);
-            Console.WriteLine("Press enter to exit...");
-            Console.ReadKey();
-            Environment.Exit(1);
-        }
-
-        public static void HandleInvalidInput()
-        {
-            Console.WriteLine("You entered an invalid value.");
-            Console.WriteLine("Press enter to exit...");
-            Console.ReadKey();
-            Environment.Exit(1);
-        }
-    }
 }
